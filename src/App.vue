d7c3100b38bbeb2faff972a1e46dd6e246eb0c0c
yuyaamano23 <nba.chako.nba@icloud.com>
2022-03-06 23:13:47 +0900

    add: import tailwindCSS

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <ul>
-    <li>
+    <li class="text-sky-400/100 underline">
       <router-link :to="{ path: '/' }">HOME</router-link>
     </li>
-    <li>
+    <li class="text-sky-400/100 underline">
       <router-link :to="{ name: 'About' }">About</router-link>
     </li>
   </ul>
   <router-view></router-view>
+  <h1 class="text-3xl font-bold">TailwindCSSの確認</h1>
 </template>
 
 <scrpt></scrpt>

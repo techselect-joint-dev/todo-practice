a0f9742889232527ad89bf3b1421320adf7bb3d0
yuyaamano23 <nba.chako.nba@icloud.com>
2022-03-07 23:57:13 +0900

    add: todoリストの雛形追加

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
 <template>
-  <ul>
-    <li class="text-sky-400/100 underline">
-      <router-link :to="{ path: '/' }">HOME</router-link>
-    </li>
-    <li class="text-sky-400/100 underline">
-      <router-link :to="{ name: 'About' }">About</router-link>
-    </li>
-  </ul>
-  <router-view></router-view>
-  <h1 class="text-3xl font-bold">TailwindCSSの確認</h1>
+  <!-- ルーティングの確認のため一応残している -->
+  <!-- <ul>
+      <li class="text-sky-400/100 underline">
+        <router-link :to="{ path: '/' }">HOME</router-link>
+      </li>
+      <li class="text-sky-400/100 underline">
+        <router-link :to="{ name: 'About' }">About</router-link>
+      </li>
+    </ul>
+    <router-view></router-view> -->
+  <!-- ここからToDoを実装していく -->
+  <div class="container mx-auto py-10">
+    <TodoList :todos="todos" />
+  </div>
 </template>
 
-<scrpt></scrpt>
+<script>
+import { ref } from "vue";
+import TodoList from "./components/modules/TodoList.vue";
+
+export default {
+  name: "App",
+  components: { TodoList },
+  setup() {
+    const newTodo = ref("");
+    const todos = ref([
+      { content: "Todo1", isDone: false },
+      { content: "Todo2", isDone: true },
+      { content: "Todo3", isDone: false },
+    ]);
+    return { newTodo, todos };
+  },
+};
+</script>
---
+++ src/components/modules/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <li
+      class="row my-1 mx-auto flex w-2/4 items-center justify-between rounded border bg-gray-100 py-1 px-4 text-gray-600"
+      v-for="(todo, index) in todos"
+      :key="index"
+    >
+      <input type="checkbox" />
+      {{ todo.content }}
+      <button>削除</button>
+    </li>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TodoList",
+  props: {
+    todos: {
+      required: false,
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style></style>
